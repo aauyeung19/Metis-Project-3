6fadbffc74802c9c8edfde74c19168e7202d4771
Andrew Auyeung <andrew.k.auyeung@gmail.com>
2020-10-21 11:34:16 -0400

    Started Cleaning Hurricane Data.  Write functions to parse multiple tables from .csv

--- lib/cleaning.py
+++ lib/cleaning.py
@@ -13,6 +13,8 @@ import pickle
 import seaborn as sns
 import matplotlib.pyplot as plt
 import psycopg2 as pg
+import csv
+import math
 
 
 def make_lag(df, col_name, lags):
@@ -108,6 +110,91 @@ def get_cleaned_df():
 
     return wdf
 
+def convert_lat_long(l):
+    """
+    Converts latitude and longitude to decimal
+    """
+    l = str(l)
+    l = l.strip()
+    if 'S' in l or 'W' in l:
+        mod = -1
+    else:
+        mod = 1
+    return mod*float(l[:-1])
+
+
+def haversine(lat,lon):
+    """
+    Returns the distance of the lat/long coord from EWR
+    using the haversine formula
+    args: 
+        lat (float): latitude of storm
+        long (float): longitude of storm
+    returns:
+        distance (float): distance from EWR in kilometers
+    """
+    # Coordinates of EWR
+    ewr_coord = (40.6895, -74.1745)
+    # approximate radius of earth in km
+    R = 6373.0
+    lat1 = math.radians(lat)
+    lon1 = math.radians(lon)
+    lat2 = math.radians(ewr_coord[0])
+    lon2 = math.radians(ewr_coord[1])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+    return distance
+
+def clean_hurr():
+    """
+    Reads data from the Northeast and North Central Pacific hurricane database 
+    Note: raw data from the database contains multiple tables in one CSV
+    args:
+        None
+    returns:
+        hurr (DataFrame): cleaned dataframe
+    """
+
+    # Read hurricane data
+    hurr = convert_hurr_to_df('../src/hurdat2-1851-2019-052520.txt')
+
+    hurr.lat = hurr.lat.apply(convert_lat_long)
+    hurr.lon = hurr.lon.apply(convert_lat_long)
+    # hurr = hurr.groupby(['basin', 'name', 'date','status'], as_index=False).mean()
+    hurr.date = hurr.date.map(lambda d: d[:4] + '-' + d[4:6] + '-' + d[6:])
+    hurr.date = pd.to_datetime(hurr.date)
+    hurr['distance'] = [haversine(x,y) for x,y in zip(hurr.lat, hurr.lon)]
+
+def convert_hurr_to_df(path):
+    """
+    Function to create a dataframe from HURDAT2
+    args:
+        path(str): relative path to .txt file
+    returns:
+        df(dataframe): Dataframe file
+    """
+    with open(path, newline='') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    
+    df_list = []
+    for row in data:
+        if len(row)==4:
+            basin = row[0]
+            name = row[1].strip()
+        else:
+            curr_row = dict(zip(['date', 'time', 'record_id', 'status', 'lat', 'lon', 'max_ws', 'min_press'], row[:8]))
+            curr_row['basin'] = basin
+            curr_row['name'] = name
+            df_list.append(curr_row)
+    
+    df = pd.DataFrame(df_list)
+    return df
+
+
 if __name__ == "__main__":
 
     print('This is the cleaned DataFrame')
@@ -119,3 +206,4 @@ if __name__ == "__main__":
     noaa.rename(columns={'DATE':'date'}, inplace=True)
     noaa = parse_month_year(noaa)
     noaa = noaa[noaa.year>=1990]
+
