c27e25b16cf2e6fccff9ca8cf6e75479869028e3
Andrew Auyeung <andrew.k.auyeung@gmail.com>
2020-10-26 21:33:40 -0400

    Update Lib

--- lib/classification.py
+++ lib/classification.py
@@ -9,16 +9,19 @@ import feature_eng as fe
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, classification_report, roc_curve
+from sklearn.metrics import fbeta_score, make_scorer 
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
 from xgboost import XGBClassifier
 from imblearn.over_sampling import RandomOverSampler, SMOTE
 from imblearn.pipeline import Pipeline as imbPipeline
+
+f2scorer = make_scorer(fbeta_score, beta=1.5)
 
 def baseline_X_y():
     # query = "SELECT * FROM daily WHERE date>'2014-01-01' ORDER BY date;"
@@ -59,6 +62,9 @@ def baseline_classifiers(X_train, y_train, X_test, y_test, threshold = 0.5):
         X_train, y_train (array): Train dataset
         X_test, y_test (array): Test dataset
     """
+
+    from sklearn.metrics import roc_curve, confusion_matrix, classification_report
+
     # All Predictions is Raining 
     rain_preds = np.ones(len(X_test))
 
@@ -96,7 +102,6 @@ def baseline_classifiers(X_train, y_train, X_test, y_test, threshold = 0.5):
     print('logreg confusion matrix: \n',confusion_matrix(y_test, logreg_preds),'\n', classification_report(y_test, logreg_preds))
     print('random forest matrix: \n',confusion_matrix(y_test, rf_preds),'\n', classification_report(y_test, rf_preds))
     print('XGBoost matrix: \n',confusion_matrix(y_test, xgb_preds),'\n', classification_report(y_test, xgb_preds))
-
     
     fpr_knn, tpr_knn, _ = roc_curve(y_test, knn_proba, pos_label=1)
     fpr_logreg, tpr_logreg, _ = roc_curve(y_test, logreg_proba, pos_label=1)
@@ -131,12 +136,12 @@ def knn_cv(X_train, y_train, cv=5, verbose=False):
     """
     pipe = imbPipeline(steps=[
         ('sample', SMOTE()), 
-        ('scaler', StandardScaler()), 
+        ('scaler', MinMaxScaler()), 
         ('knn', KNeighborsClassifier())
         ])
-    params = [{'knn__n_neighbors': range(2,50), 'knn__p': [1, 2]}]
+    params = [{'knn__n_neighbors': range(2,25), 'knn__p': [1, 2]}]
 
-    model = GridSearchCV(pipe, params, cv=cv, n_jobs=-1, scoring='recall', verbose=verbose)
+    model = GridSearchCV(pipe, params, cv=cv, n_jobs=-1, scoring=f2scorer, verbose=verbose)
     model.fit(X_train, y_train)
     return model
 
@@ -152,18 +157,18 @@ def logreg_cv(X_train, y_train, cv=5, verbose=False):
         model (estimator): best estimator with highest recall
     """
     logreg = LogisticRegression()
-    weights = np.linspace(0.05, 0.95, 10)
+    # weights = np.linspace(0.05, 0.95, 10)
     params = [{
         'logreg__penalty': ['l1', 'l2', 'elasticnet', 'none'],
         'logreg__C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 
-        'logreg__class_weight': [{0:x, 1:1-x} for x in weights]
+        # 'logreg__class_weight': [{0:x, 1:1-x} for x in weights]
 
         }]
     pipe = imbPipeline(steps=[
         ('sample', SMOTE()), 
         ('logreg', logreg)
         ])
-    model = GridSearchCV(pipe, params, cv=cv, n_jobs=-1, scoring='recall', verbose=verbose)
+    model = GridSearchCV(pipe, params, cv=cv, n_jobs=-1, scoring=f2scorer, verbose=verbose)
     model.fit(X_train, y_train)
     return model
 
@@ -179,17 +184,15 @@ def rf_cv(X_train, y_train, cv=5, verbose=False):
         model (estimator): best estimator with highest recall
     """
     rf = RandomForestClassifier()
-    weights = np.linspace(0.05, 0.95, 10)
     params = [{
-        'rf__n_estimators': range(50, 550, 50),
+        'rf__n_estimators': range(50, 450, 50),
         'rf__max_depth': [5, 10], 
         'rf__min_samples_split': [2, 10, 20],
         'rf__max_features': ['sqrt', 8, 10],
         'rf__criterion': ['gini'], 
-        'rf__class_weight': [{0:x, 1:1-x} for x in weights]
         }]
     pipe = imbPipeline(steps=[('sample', SMOTE()), ('rf', rf)])
-    model = GridSearchCV(pipe, params, cv=cv, n_jobs=-1, scoring='recall', verbose=verbose)
+    model = GridSearchCV(pipe, params, cv=cv, n_jobs=-1, scoring=f2scorer, verbose=verbose)
     model.fit(X_train, y_train)
     return model
 
@@ -208,14 +211,14 @@ def xgb_cv(X_train, y_train, X_test, y_test, cv=5, verbose=False):
     params = [{
         'n_estimators': [1000], 
         'max_depth': [3, 5, 7, 9],
-        'learning_rate': [0.01, 0.02, 0.03, 0.05, 0.1],
+        'learning_rate': [0.03, 0.05, 0.1],
         'subsample': [0.25, 0.5, 1], 
         'min_child_weight': [1],
         'colsample_bytree': [.8],
         'scale_pos_weight': [1, 10, 25, 50, 75, 99]
     }]
     fit_params={
-        "early_stopping_rounds":20, 
+        "early_stopping_rounds":30, 
         "eval_metric" : "auc", 
         "eval_set" : [[X_test, y_test]],
         "verbose": verbose
@@ -229,6 +232,24 @@ def xgb_cv(X_train, y_train, X_test, y_test, cv=5, verbose=False):
     model.fit(X_train, y_train, **fit_params)
     return model
 
+def vote(X_train, y_train, estimators, **kwargs):
+    """
+    Stacks models based on predictions given from past iterations.
+    args:
+        X_train (array): Train dataset with Features
+        y_train (array): Train Target
+        cv (object): Default 5-Fold CrossValidation
+        Verbose (bool): True to see verbose GridSearchCV 
+        estimators (list): List of trained estimators
+    returns:
+        model (estimator): best estimator with highest recall
+    """
+    model = VotingClassifier(
+        estimators=estimators,  
+        n_jobs=-1,
+        **kwargs)
+    model.fit(X_train, y_train)
+    return model
 # REDO MODELS FOR CLASS WEIGHT vs SMOTE
 # Random Forest: class_weight={0:1, 1:2}
 # Doubles the weight of rain train data
