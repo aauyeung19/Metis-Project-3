8ec8f226e3695a36a9e09a2570878e4ae763a9be
Andrew Auyeung <andrew.k.auyeung@gmail.com>
2020-11-04 16:35:41 -0500

    added streamlit folder

---
+++ streamlit/app.py
@@ -0,0 +1,116 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import owm
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import matplotlib.dates
+import datetime
+import seaborn as sns
+
+@st.cache(allow_output_mutation=True)
+def load_models():
+    v = 5
+    vote = pickle.load(open(f'../Version {v}/vote.pickle', 'rb'))
+    knn = pickle.load(open(f'../Version {v}/knn.pickle', 'rb'))
+    logreg = pickle.load(open(f'../Version {v}/logreg.pickle', 'rb'))
+    rf = pickle.load(open(f'../Version {v}/rf.pickle', 'rb'))
+    xgb = pickle.load(open(f'../Version {v}/xgb.pickle', 'rb'))
+    model_dict = {'Voting': vote, 'KNearestNeighbors': knn.best_estimator_, 'LogisticRegression': logreg.best_estimator_, 'Random Forest': rf.best_estimator_, 'XGBoost': xgb.best_estimator_}
+    
+    loc_dict = {
+        'Newark': [40.6895, -74.1745], 
+        'Chicago': [41.9742, -87.9073], 
+        'Raleigh': [35.8801, -78.7880],
+        'Seattle': [47.6062, -122.3321],
+        'Los Angeles': [34.0522, -118.2437],
+        'Atlanta': [33.7490, -84.3880],
+        'Washington DC': [38.9072, -77.0369],
+        'Dallas': [32.7767, -96.7970],
+        'San Francisco': [37.7749, 122.4194]}
+    
+    train_cols = ['temp_avg', 'ws_avg', 'press_avg', 'humid_avg', 'dp_avg', 'under_dp',
+        'temp_kelvin', 'month', 'PRCP', 'SNOW', 'lat', 'lon', 'distance',
+        'distance_delta', 'press_delta', 'humid_delta', 'ws_delta',
+        'temp_trend', 'humid_trend', 'month_1', 'month_2', 'month_3', 'month_4',
+        'month_5', 'month_6', 'month_7', 'month_8', 'month_9', 'month_10',
+        'month_11', 'month_12']
+
+    data = {}
+    for curr_loc in loc_dict:
+        lat, lon = loc_dict[curr_loc][0], loc_dict[curr_loc][1]
+        # Ping API
+        df = owm.get_owm(lat, lon)
+        # Add dummies
+        df[['month_1', 'month_2', 'month_3', 'month_4', 'month_5', 'month_6', 'month_7', 'month_8', 'month_9', 'month_10', 'month_11', 'month_12']] = 0
+        for each in df.month:
+            df['month_'+str(each)] = 1
+        # Assume no current storm
+        df[['lat', 'lon', 'distance', 'distance_delta']] = -9999
+        # Add all predictions and probabilities to the dictionairy
+        # Keys are the names of the models
+        preds_dict = {}
+        for model in model_dict:
+            preds = model_dict[model].predict(df[train_cols].dropna())[-7:]
+            proba = model_dict[model].predict_proba(df[train_cols].dropna())[-7:,1]
+            preds_dict[model] = {'preds': preds, 'proba': proba}
+        # Add predictions and probabilities to current location key in Dictionairy 
+        data[curr_loc] = preds_dict
+        data[curr_loc]['actual'] = df
+        
+    return data
+
+
+
+
+if __name__=="__main__":
+    
+    cached_data = load_models()
+    st.title('rain.One')
+    st.markdown('### Seven Day Forecast')
+    # Select Location
+    locations = ['Newark', 'Chicago', 'Raleigh', 'Seattle', 'Los Angeles', 'Atlanta', 'Washington DC', 'Dallas'] # , 'San Francisco']
+    loc = st.selectbox('Pick a location', locations)
+    df = cached_data[loc]['actual'].copy(deep=True)
+    df.sort_values(by='date', inplace=True)
+    df.date = df.date.map(lambda x: x.day_name())
+    # Selection of Models 
+    models = ['Voting', 'KNearestNeighbors', 'LogisticRegression', 'Random Forest', 'XGBoost']
+    show_all = st.checkbox('Show All Estimators', value=False)
+    if show_all:
+        all_df = pd.DataFrame()
+        for m in models:
+            curr_df = pd.DataFrame(zip(df['date'].iloc[6:], cached_data[loc][m]['proba']), columns=['Date', 'Probability'])
+            curr_df['Model'] = m
+            all_df = all_df.append(curr_df, ignore_index=True)
+        custom_palette = ["#029E73", "#D55E00", "#CC78BC",
+            "#CA9161", "#FBAFE4", "#949494", "#ECE133", "#56B4E9"]
+        fig = sns.catplot(x='Date', y='Probability', data=all_df, kind='strip', hue='Model', legend=False, palette=custom_palette, height=5, aspect=1.5)
+        plt.plot(df['date'].iloc[6:], cached_data[loc]['Voting']['preds'], label='Prediction', linewidth=4)
+        plt.plot(df['date'].iloc[6:], df['pop'].tail(7), label='Forecast', color='darkorange', linewidth=2) ##DE8F05
+        plt.xticks(fontsize=12)
+        plt.yticks(fontsize=12)
+        plt.xlabel('Date', fontsize=12)
+        plt.ylabel('Probability', fontsize=12)
+        plt.title('Comparisons of Models against Prediction from OpenWeatherMap', fontsize=16)
+        plt.legend(bbox_to_anchor=(1,0.7))
+        plt.ylim((0,1.1))
+        st.pyplot(fig)
+    else:
+        model = st.selectbox('Select an Estimator', models)
+        preds = cached_data[loc][model]['preds']
+        proba = cached_data[loc][model]['proba']
+        pred_type = st.selectbox("How would you like to show the model's prediction?", ['Prediction', 'Probability'])
+        fig = plt.figure()
+        if pred_type == 'Prediction':
+            plt.plot(df['date'].iloc[6:], preds, label='Prediction', linewidth=4)
+        else:
+            plt.plot(df['date'].iloc[6:], proba, label='Prediction Chance', linewidth=4)
+        plt.plot(df['date'].iloc[6:], df['pop'].tail(7), label='Forecast')
+        plt.ylim(0,1.1)
+        plt.xlabel('Date')
+        plt.ylabel('Chance of Rain')
+        plt.xticks(rotation=90)
+        plt.title(f'Prediction of Rain in {loc} Using {model} Estimator')
+        plt.legend(bbox_to_anchor=(1,0.7))
+        st.pyplot(fig)
---
+++ streamlit/owm.py
@@ -0,0 +1,134 @@
+"""
+Author: @Andrew Auyeung
+
+Queries openweathermap.org API to collect 
+historical 5 day data and 7 day forecast
+
+Documentation: 
+https://openweathermap.org/api/one-call-api
+"""
+
+import requests
+import pandas as pd
+import numpy as np
+import feature_eng as fe
+import datetime as dt
+import pickle
+
+# Get current date
+# Loop through date - date-5
+
+def parse_month_year(df):
+    """
+    Parse date to Month and Years
+    """
+    df['year'] = df.date.map(lambda x: x.year)
+    df['month'] = df.date.map(lambda x: x.month)
+    return df
+
+def convert_to_kelvin(temp_col):
+    temp_col = temp_col.map(lambda x: (x - 32) * 5/9 + 273.15)
+    return temp_col
+
+
+def get_owm(lat,lon):
+    """
+    Gets 5 day history and 7 day forecast from OpenWeatherMap.org
+    args:
+        lat (float): Latitude
+        lon (float): Longitude
+    returns:
+        owm_df (DataFrame): DataFrame with OpenWeatherMap data
+    """
+    # get current date
+    now = dt.datetime.now()
+
+    #start empty dataframe
+    history_list = []
+
+    #### Get History
+    # Loop through 5 previous days
+    for days in range(1,6):
+        curr_day = int((now - dt.timedelta(days)).timestamp())
+        url = f'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={curr_day}&units=imperial&appid=98ec6864b86b42efec56dc8a1b9abcef'
+        
+        page = requests.get(url)
+
+        day_dict = page.json()
+        today = day_dict['current']
+        history_list.append(today)
+    
+    # owm_df = pd.DataFrame(history_list)
+
+    #### Get Forecast
+
+    url = f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,alerts&units=imperial&appid=98ec6864b86b42efec56dc8a1b9abcef'
+    page = requests.get(url)
+    forecast = page.json()
+    
+    # Add today's data
+    history_list.append(forecast['current'])
+    owm_df = pd.DataFrame(history_list)
+    owm_df['rain'] = np.zeros(len(owm_df))
+    # Add Forecast
+    
+    forecast = forecast['daily']
+    forecast = pd.DataFrame(forecast)
+    forecast.temp = [t['day'] for t in forecast.temp]
+    forecast.feels_like = [f['day'] for f in forecast.feels_like]
+    owm_df = pd.concat([owm_df, forecast], ignore_index=True)
+
+    owm_df.drop(columns='weather', inplace=True)
+    try:
+        owm_df.drop_duplicates(inplace=True)
+    except:
+        pass
+    
+    # Clean owm_df
+    owm_df.dt = owm_df.dt.map(dt.datetime.fromtimestamp)
+    owm_df.pressure = owm_df.pressure.map(lambda x: x/33.86)
+
+    # rename columns to match columns from model
+    owm_df.rename(
+        columns={
+            'dt':'date',
+            'temp': 'temp_avg',
+            'pressure': 'press_avg',
+            'humidity': 'humid_avg',
+            'dew_point': 'dp_avg',
+            'wind_speed': 'ws_avg',
+            'rain': 'PRCP'
+        },
+        inplace=True)
+    owm_df['day'] = owm_df.date.map(lambda x: x.day)
+    owm_df.drop_duplicates('day', inplace=True)
+    # owm_df.head()
+    # Clean data
+    owm_df = parse_month_year(owm_df)
+    owm_df['temp_kelvin'] = convert_to_kelvin(owm_df.temp_avg)
+    owm_df.sort_values(by='date', ascending=True, inplace=True)
+
+    owm_df['press_delta'] = owm_df.press_avg.diff()
+    owm_df['humid_delta'] = owm_df.humid_avg.diff()
+    owm_df['ws_delta'] = owm_df.ws_avg.diff()
+
+    owm_df['temp_trend'] = fe.ma_shifts(3, range(1,5), owm_df, 'temp_kelvin').iloc[:,2:].sum(axis=1)
+    owm_df['humid_trend'] = fe.ma_shifts(3, range(1,5), owm_df, 'humid_avg').iloc[:,2:].sum(axis=1)
+    owm_df['press_trend'] = fe.ma_shifts(3, range(1,5), owm_df, 'press_avg').iloc[:,2:].sum(axis=1)
+    owm_df['PRCP'].fillna(0, inplace=True)
+    owm_df['pop'].fillna(0, inplace=True)
+    owm_df['SNOW'] = np.zeros(len(owm_df))
+    owm_df['under_dp'] = (owm_df['temp_avg'] <= owm_df['dp_avg']).astype(int)
+    owm_df.drop(columns=['day', 'visibility', 'year'], inplace=True)
+    
+    return owm_df
+
+if __name__=='__main__':
+    # Test functions
+    ewr = [40.6895, -74.1745]
+    rdu = [35.8801, -78.7880]
+    lat = float(rdu[0])
+    lon = float(rdu[1])
+    df = get_owm(lat, lon)
+    print('This is the data from RDU')
+    df.head()
